f701eac946aca8d354face31d6658c29d1dd7ead
G-Web-Essentials <117399636+G-Web-Essentials@users.noreply.github.com>
2023-05-12 22:16:34 +0100

    Sign in and Registration updated with venue and users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def tabbed_menu_item(text, link)
+  active_class =
+    if request.path.include?(link)
+      'active'
+    else
+      ''
+    end
+
+  link_to text, link, class: active_class
+end
 end

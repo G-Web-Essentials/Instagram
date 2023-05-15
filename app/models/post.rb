51b558f1df05f4ec008d3392a24ec9b6d14b9640
G-Web-Essentials <117399636+G-Web-Essentials@users.noreply.github.com>
2023-05-15 18:21:22 +0100

    Added likes to posts, including double click to like.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :venue
   has_many_attached :images
+  has_many :likes, :dependent => :delete_all 
+  has_many :likers, through: :likes, source: :user, :dependent => :delete_all 
     
   validates :images, presence: false, blob: { content_type: :image, size_range: 1..(5.megabytes) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_130553) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_151337) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_130553) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_animals_on_user_id"
     t.index ["venue_id"], name: "index_animals_on_venue_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -98,5 +108,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_130553) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "animals", "users"
   add_foreign_key "animals", "venues"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "venues"
 end
